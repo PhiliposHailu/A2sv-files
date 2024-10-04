f4b1e041693c442beef6fdc62bd175ed36c49c13
@PhiliposHailu <148946615+PhiliposHailu@users.noreply.github.com>
2024-10-04 08:13:14 +0300

    Create 7. Convert_the_Temperature.py

---
+++ 7. Convert_the_Temperature.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        
+        temperature = []
+        
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.80 +32.00
+        
+        temperature.append(kelvin)
+        temperature.append(fahrenheit)
+        
+        return temperature
+        
